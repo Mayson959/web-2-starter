dbbd3c78b57be42b4d6a8d94b321026dbe781585
Mayson959 <rmaysonet@vcd-ch204-05.eastern.ewu.edu>
2017-09-28 13:59:15 -0700

    Starter

---
+++ js/plugin.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Web 2 Starter</title>
+    
+    <link rel="stylesheet" href=".css">
+    
+</head>
+<body>
+    
+    <h1>Web 2 Starter</h1>
+    
+<!--  jquert Library  -->
+    <script>src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"
+    </script>
+</body>
+</html>
